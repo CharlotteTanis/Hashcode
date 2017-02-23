33e84f3c147b0dfeb6fb38b82e596f0092ec0360
MirjaLagerwaard <mirja_1994@hotmail.com>
2017-02-23 18:02:35 +0100

    Made new map for real hashcode

---
+++ pizza/algorithm.py
@@ -0,0 +1,8 @@
+from main import *
+
+def slicePizza(cells, min_ingr):
+
+        ur_corner = pizza[(0,0)]
+
+
+slicePizza(5,1)
---
+++ pizza/main.py
@@ -0,0 +1,25 @@
+import csv
+
+fp = "small.csv"
+pizza = {}
+
+def Preparefile(fp, column):
+
+
+    with open(fp, "rb") as filePizza:
+
+        reader_ingredients = csv.reader(filePizza, delimiter=",")
+
+        r = 0
+
+        for ingredients in reader_ingredients:
+            for c in range(0, column):
+                pizza[(r, c)] = ingredients[c]
+            r += 1
+
+    # for i in range(0, 6):
+    #         print pizza[(i,0)], pizza[(i,1)], pizza[(i,2)], pizza[(i,3)], pizza[(i,4)], pizza[(i,5)], pizza[(i,6)]
+
+    # print pizza
+
+Preparefile(fp, 7)
