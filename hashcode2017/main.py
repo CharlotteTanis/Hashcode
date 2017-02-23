1d0ba75c86cbb8fb4fa38432fc62a1e291ae95a1
MirjaLagerwaard <mirja_1994@hotmail.com>
2017-02-23 20:22:40 +0100

    Almost first solution

--- hashcode2017/main.py
+++ hashcode2017/main.py
@@ -1,42 +1,49 @@
 import csv
 
-fp = "kittens.in"
+fp = "me_at_the_zoo.in"
 
 def Preparefile(fp):
-
+    videos_list = {}
 
     with open(fp, "rb") as fileVideos:
 
         reader_videos = csv.reader(fileVideos, delimiter=" ")
 
+        counter = 0
         for videos in reader_videos:
-            V = videos[0]
-            E = videos[1]
-            R = videos[2]
-            C = videos[3]
-            X = videos[4]
+            if counter == 0:
+                V = int(videos[0])
+                E = int(videos[1])
+                R = int(videos[2])
+                C = int(videos[3])
+                X = int(videos[4])
+                counter += 1
+                continue
+
+            if counter == 1:
+                for i in range(0, V):
+                    videos_list[i] = int(videos[i])
+                break
+
+    servers = {}
+    server_id = 0
+    mbs = 0
+    temp_vid = []
+    for video in videos_list:
+        if server_id > (C - 1):
             break
+        if mbs < X:
+            mbs += videos_list[video]
+            temp_vid.append(video)
+            continue
 
-        for videos in reader_videos:
-            
+        servers[server_id] = temp_vid
+        server_id += 1
+        temp_vid = []
+        mbs = 0
 
-
-        # print reader_videos[0]
-
-        #
-        #
-        # r = 0
-        #
-        # for videos in reader_videos:
-        #     for c in range(0, column):
-        #         video[c] = V
-        #
-        #         pizza[(r, c)] = ingredients[c]
-        #     r += 1
-
-    # for i in range(0, 6):
-    #         print pizza[(i,0)], pizza[(i,1)], pizza[(i,2)], pizza[(i,3)], pizza[(i,4)], pizza[(i,5)], pizza[(i,6)]
-
-    # print pizza
+    print server_id
+    for server in servers:
+        print server, servers[server][1:]
 
 Preparefile(fp)
